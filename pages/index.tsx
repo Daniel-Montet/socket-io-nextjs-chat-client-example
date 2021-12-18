11d2759fa2c88effa056d263121a56e73ff6f599
Daniel Montet <dmontetproff@gmail.com>
2021-12-18 13:12:31 +0300

    Found using material ui a bit of a headache, not worth the trouble for such a small project hence replaced it with tailwind instead. Ive creeted a header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,24 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
-import { AccountCircle } from "@mui/icons-material";
-
-const theme = createTheme();
-
 export default function Home() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box
-        sx={{
-          backgroundColor: "#AEB5C2",
-          height: "100vh",
-          width: "100vw",
-          paddingLeft: 2,
-          paddingRight: 2,
-        }}
-      >
-        <Grid container>
-          <Grid item xs={12}>
-            <Avatar alt="" src="/logo.png" />
-          </Grid>
-          <Grid item xs={4}>
-            <TextField
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item xs={8}>
-            //clearchat more
-          </Grid>
-        </Grid>
-      </Box>
-    </ThemeProvider>
+    <main className="px-10 pt-5 bg-secondary h-screen">
+      <section className="header grid grid-cols-2">
+        <div className="logo flex">
+          <img src="/logo.png" alt="" className="h-10" />
+          <p className="font-semibold self-center ml-5">ChatAPP</p>
+        </div>
+        <img
+          src="/notification.png"
+          alt=""
+          className="notify self-center justify-self-end h-5"
+        />
+      </section>
+      <section className="main">
+        <section className="utils"></section>
+        <section className="content">
+          <section className="nav"></section>
+          <section className="chat"></section>
+        </section>
+      </section>
+    </main>
   );
 }
