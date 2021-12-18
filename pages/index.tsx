b90ade9fa39118b93f5378ff1d4c85d00e32cdf4
Daniel Montet <dmontetproff@gmail.com>
2021-12-18 07:40:59 +0300

    removed grid spacing and container from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,50 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import { AccountCircle } from "@mui/icons-material";
 
 const theme = createTheme();
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth={false}>
-        <Grid container spacing={2}>
+      <CssBaseline />
+      <Box
+        sx={{
+          backgroundColor: "#AEB5C2",
+          height: "100vh",
+          width: "100vw",
+          paddingLeft: 2,
+          paddingRight: 2,
+        }}
+      >
+        <Grid container>
           <Grid item xs={12}>
             <Avatar alt="" src="/logo.png" />
           </Grid>
           <Grid item xs={4}>
-            // search
+            <TextField
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+            />
           </Grid>
           <Grid item xs={8}>
             //clearchat more
           </Grid>
         </Grid>
-      </Container>
+      </Box>
     </ThemeProvider>
   );
 }
