a31e3033cfc472e2f8bdf506aa6c08290f2d793b
Daniel Montet <dmontetproff@gmail.com>
2022-01-22 18:34:37 +0300

    Add correct environment variable syntax

--- lib/socketClient.ts
+++ lib/socketClient.ts
@@ -8,11 +8,9 @@ interface socket extends Socket<DefaultEventsMap, DefaultEventsMap> {
 	userID?: string
 }
 
-const server_url = process.env.NODE_ENV === "production" ? process.env.SERVER_URL : 'http://localhost:8000';
-// const local_url = 'http://localhost:8000'
+const server_url = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:8000';
 console.log(process.env.NODE_ENV)
-console.log(process.env.SERVER_URL)
-console.log(server_url)
+console.log(process.env.NEXT_PUBLIC_SERVER_URL)
 const socket: socket = io(server_url!, { autoConnect: false });
 
 
