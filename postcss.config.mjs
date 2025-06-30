deb74e31a823087d84e2d0291160fd15a6b12c68
Daniel Montet <daniel.montet@oldmutual.com>
2025-06-30 13:51:02 +0300

    fix: overhaul of packages

---
+++ postcss.config.mjs
@@ -0,0 +1,13 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+export default config;
